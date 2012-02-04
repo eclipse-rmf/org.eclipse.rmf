3fe58f3c8b582708e226ba8eb92461437bbcf9d2
Mark Broerkens <mark.broerkens@itemis.de>
2012-02-04 04:30:53 +0100

    ADDED: TestSuit classes
    allow for calling multiple tests automatically from headless build

---
+++ org.eclipse.rmf.reqif10.model.test/src/org/eclipse/rmf/reqif10/model/test/AllTests.java
@@ -0,0 +1,14 @@
+package org.eclipse.rmf.reqif10.model.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+@RunWith(Suite.class)
+@SuiteClasses({ SpecObjectTest.class, SpecRelationTest.class })
+public class AllTests {
+	
+
+
+}
---
+++ org.eclipse.rmf.rif12.model.test/src/org/eclipse/rmf/rif12/model/test/AllTests.java
@@ -0,0 +1,17 @@
+package org.eclipse.rmf.rif12.model.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+@RunWith(Suite.class)
+@SuiteClasses({ DatatypeDefinitionNonValidatingEnumTest.class, DatatypeNodeFactoryTest.class, DoorsExportTest.class, EnumDatatypeNodeFactoryTest.class, ExportSpecGroupTest.class, ExportSpecTypeSimpleAttributeDefinitionTest.class, 
+	ExportWithDeletedSpecObjectTest.class, ImportProjectTest.class, MultiReferenceTest.class, ProrExportTest.class, ProRToolExtensionTest.class, ReImportOfExportTest.class, RifNodeFactoryTest.class, RifPerformanceTest.class, SpecGroupTest.class, 
+	SpecGroupWithInvalidReferenceTest.class, SpecObjectAttributeValueTest.class, SpecObjectTest.class, SpecRelationTest.class, SpecTypeEnumerationAttributeDefinitionTest.class, SpecTypeSimpleAttributeDefinitionTest.class,
+	SpecTypeSimpleAttributeDefinitionWithEmptyDefaultValueTest.class, SpecTypeSimpleAttributeDefinitionWithoutDefaultValueTest.class, ToolExtensionTest.class})
+public class AllTests {
+	
+
+
+}
