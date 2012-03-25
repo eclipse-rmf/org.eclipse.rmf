8472711f1dd8583ff0a95a0d818101b0c4ce8d93
Mark Broerkens <mark.broerkens@itemis.de>
2012-03-26 00:04:44 +0200

    NEW - bug 370665: optimize serialization mechanism https://bugs.eclipse.org/bugs/show_bug.cgi?id=370665

---
+++ org.eclipse.rmf.reqif10.tests/src/org/eclipse/rmf/reqif10/tests/TC3000ModelBuilder.java
@@ -0,0 +1,178 @@
+/**
+ * Copyright (c) 2012 itemis AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Mark Bršrkens - initial API and implementation
+ * 
+ */
+package org.eclipse.rmf.reqif10.tests;
+
+import java.math.BigInteger;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.rmf.reqif10.AttributeDefinitionBoolean;
+import org.eclipse.rmf.reqif10.AttributeDefinitionDate;
+import org.eclipse.rmf.reqif10.AttributeDefinitionEnumeration;
+import org.eclipse.rmf.reqif10.AttributeDefinitionInteger;
+import org.eclipse.rmf.reqif10.AttributeDefinitionReal;
+import org.eclipse.rmf.reqif10.AttributeDefinitionString;
+import org.eclipse.rmf.reqif10.AttributeDefinitionXHTML;
+import org.eclipse.rmf.reqif10.AttributeValueBoolean;
+import org.eclipse.rmf.reqif10.AttributeValueDate;
+import org.eclipse.rmf.reqif10.AttributeValueEnumeration;
+import org.eclipse.rmf.reqif10.AttributeValueInteger;
+import org.eclipse.rmf.reqif10.AttributeValueReal;
+import org.eclipse.rmf.reqif10.AttributeValueString;
+import org.eclipse.rmf.reqif10.AttributeValueXHTML;
+import org.eclipse.rmf.reqif10.DatatypeDefinition;
+import org.eclipse.rmf.reqif10.DatatypeDefinitionBoolean;
+import org.eclipse.rmf.reqif10.DatatypeDefinitionDate;
+import org.eclipse.rmf.reqif10.DatatypeDefinitionEnumeration;
+import org.eclipse.rmf.reqif10.DatatypeDefinitionInteger;
+import org.eclipse.rmf.reqif10.DatatypeDefinitionReal;
+import org.eclipse.rmf.reqif10.DatatypeDefinitionString;
+import org.eclipse.rmf.reqif10.DatatypeDefinitionXHTML;
+import org.eclipse.rmf.reqif10.EmbeddedValue;
+import org.eclipse.rmf.reqif10.EnumValue;
+import org.eclipse.rmf.reqif10.ReqIF10Factory;
+import org.eclipse.rmf.reqif10.SpecHierarchy;
+import org.eclipse.rmf.reqif10.SpecObject;
+import org.eclipse.rmf.reqif10.SpecObjectType;
+import org.eclipse.rmf.reqif10.Specification;
+import org.eclipse.rmf.reqif10.SpecificationType;
+import org.eclipse.rmf.reqif10.XhtmlContent;
+import org.eclipse.rmf.reqif10.xhtml.XhtmlDivType;
+import org.eclipse.rmf.reqif10.xhtml.XhtmlFactory;
+import org.eclipse.rmf.reqif10.xhtml.XhtmlH1Type;
+
+public class TC3000ModelBuilder extends SimpleModelBuilder {
+
+	// datatypes
+	DatatypeDefinitionXHTML datatypeDefinitionXHTML;
+
+	// SpecObjectTypes
+	SpecObjectType specObjectType;
+	AttributeDefinitionXHTML attributeDefinitionXHTML;
+
+
+	// SpecificationTypes
+	SpecificationType specificationType;
+
+	// SpecObjects
+	SpecObject specObject;
+
+	/**
+	 * 
+	 */
+	public TC3000ModelBuilder() {
+		super(new Date(), "ID_TC3000_ReqIfHeader", "TC 3000 'Formated Content'");
+	}
+
+	public TC3000ModelBuilder(Date creationTime, String identifier, String title) {
+		super(creationTime, identifier, title);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createDatatypes() throws DatatypeConfigurationException {
+		datatypeDefinitionXHTML = ReqIF10Factory.eINSTANCE
+				.createDatatypeDefinitionXHTML();
+		EList<DatatypeDefinition> datatypes = getReqIF().getCoreContent()
+				.getDatatypes();
+		datatypes.add(datatypeDefinitionXHTML);
+	}
+
+	@Override
+	public void createSpecObjectTypes() throws DatatypeConfigurationException {
+		specObjectType = ReqIF10Factory.eINSTANCE.createSpecObjectType();
+		specObjectType.setIdentifier("ID_TC3000_SpecObjectType");
+		specObjectType.setLongName("TC3000 SpecObjectType");
+		System.out.println("last Change Date: " + getLastChangeDate());
+		specObjectType.setLastChange(getLastChangeDate());
+
+		attributeDefinitionXHTML = ReqIF10Factory.eINSTANCE
+				.createAttributeDefinitionXHTML();
+
+		specObjectType.getSpecAttributes().add(
+				attributeDefinitionXHTML);
+
+		getReqIF().getCoreContent().getSpecTypes().add(specObjectType);
+
+	}
+
+	@Override
+	public void createSpecificationTypes() throws DatatypeConfigurationException {
+		specificationType = ReqIF10Factory.eINSTANCE.createSpecificationType();
+		specificationType.setIdentifier("ID_TC3000_SpecificationType");
+		specificationType.setLongName("TC3000 SpecificationType");
+		specificationType.setLastChange(getLastChangeDate());
+		getReqIF().getCoreContent().getSpecTypes().add(specificationType);
+	}
+
+	@Override
+	public void createSpecRelationGroups() {
+		// TODO Auto-generated method stub
+		super.createSpecRelationGroups();
+	}
+
+	@Override
+	public void createSpecifications() throws DatatypeConfigurationException {
+		Specification specification = ReqIF10Factory.eINSTANCE
+				.createSpecification();
+		specification.setIdentifier("ID_TCs000_Specification");
+		specification.setType(specificationType);
+		specification.setLastChange(getLastChangeDate());
+
+		SpecHierarchy specHierarchy = ReqIF10Factory.eINSTANCE
+				.createSpecHierarchy();
+		specHierarchy.setIdentifier("ID_TC3000_SpecHierarchy");
+		specHierarchy.setLongName("TC3000 SpecHierarchy");
+		specHierarchy.setLastChange(getLastChangeDate());
+		specHierarchy.setObject(specObject);
+		specification.getChildren().add(specHierarchy);
+
+		getReqIF().getCoreContent().getSpecifications().add(specification);
+
+	}
+
+	@Override
+	public void createSpecObjects() throws DatatypeConfigurationException {
+		specObject = ReqIF10Factory.eINSTANCE.createSpecObject();
+		specObject.setIdentifier("ID_TC3000_SpecObject");
+		specObject.setLastChange(getLastChangeDate());
+		specObject.setType(specObjectType);
+
+		AttributeValueXHTML attributeValueXHTML = ReqIF10Factory.eINSTANCE
+				.createAttributeValueXHTML();
+		XhtmlContent xhtmlContent = ReqIF10Factory.eINSTANCE.createXhtmlContent();
+		attributeValueXHTML.setTheValue(xhtmlContent);
+		
+		XhtmlDivType div = XhtmlFactory.eINSTANCE.createXhtmlDivType();
+		xhtmlContent.setDiv(div);
+		
+		XhtmlH1Type h1 = XhtmlFactory.eINSTANCE.createXhtmlH1Type();
+		div.getH1().add(h1);
+		
+		specObject.getValues().add(attributeValueXHTML);
+		getReqIF().getCoreContent().getSpecObjects().add(specObject);
+
+	}
+
+
+	@Override
+	public void createSpecRelations() {
+		// TODO Auto-generated method stub
+		super.createSpecRelations();
+	}
+
+}
---
+++ org.eclipse.rmf.reqif10.tests/src/org/eclipse/rmf/reqif10/tests/domain/TC3000FormatedContentTests.java
@@ -0,0 +1,264 @@
+/**
+ * Copyright (c) 2012 itemis AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Mark Broerkens - initial API and implementation
+ * 
+ */
+package org.eclipse.rmf.reqif10.tests.domain;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+
+import junit.framework.Assert;
+
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.eclipse.rmf.reqif10.ReqIF;
+import org.eclipse.rmf.reqif10.ReqIFContent;
+import org.eclipse.rmf.reqif10.ReqIF10Package;
+import org.eclipse.rmf.reqif10.SpecObject;
+import org.eclipse.rmf.reqif10.SpecObjectType;
+import org.eclipse.rmf.reqif10.SpecType;
+import org.eclipse.rmf.reqif10.tests.TC3000ModelBuilder;
+import org.eclipse.rmf.serialization.ReqIFResourceFactoryImpl;
+import org.eclipse.rmf.serialization.ReqIFResourceSetImpl;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TC3000FormatedContentTests {
+	static ReqIF originalReqIF = null;
+	static ReqIF loadedReqIF = null;
+	
+	static Resource loadedReqIFResource = null;
+	
+	
+	@BeforeClass
+	public static void setupOnce() throws Exception {
+
+		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put(
+				"ReqIF", new ReqIFResourceFactoryImpl());
+		ResourceSet rifResourceSet = new ReqIFResourceSetImpl();
+
+		rifResourceSet.getResourceFactoryRegistry()
+				.getProtocolToFactoryMap()
+				.put(ReqIF10Package.eNS_URI, ReqIF10Package.eINSTANCE);
+		originalReqIF = new TC3000ModelBuilder().getReqIF();
+		saveReqIFFile(originalReqIF, "TC3000.ReqIF");
+		loadedReqIF = loadReqIFFile("TC3000.ReqIF");
+
+	}
+	
+	private static final String WORKING_DIRECTORY = "work";
+
+	/**
+	@Test
+	public void testSchemaCompliance() throws Exception {
+		validateAgainstSchema("TC1000.ReqIF");
+	}
+	*/
+	
+	@Test
+	public void testResource() {
+		assertNotNull(loadedReqIFResource);
+		assertEquals(1,loadedReqIFResource.getContents().size());
+	} 	
+	
+	@Test
+	public void testReqIFNotNull() {
+		assertNotNull("The loaded ReqIF model shall not be Null", loadedReqIF);
+	} 
+	
+	
+	@Test
+	public void testResave() throws IOException {
+		try {
+			saveReqIFFile(loadedReqIF, "TC1000_2.ReqIF");
+		} catch (IOException ioe) {
+			Assert.assertFalse("We shall be able to save without exception. However the following exception occurred: " + ioe.toString(), true);
+		}
+	}
+	
+	@Test
+	public void testReqIF() {
+		assertTrue(loadedReqIF.eIsSet(ReqIF10Package.eINSTANCE.getReqIF_CoreContent()));
+		assertFalse(loadedReqIF.isSetLang());
+		assertTrue(loadedReqIF.eIsSet(ReqIF10Package.eINSTANCE.getReqIF_TheHeader()));
+		assertFalse(loadedReqIF.eIsSet(ReqIF10Package.eINSTANCE.getReqIF_ToolExtensions()));		
+	}
+	
+	@Test
+	public void testReqIFHeader() {
+		assertFalse(loadedReqIF.getTheHeader().isSetComment());
+		assertTrue(loadedReqIF.getTheHeader().isSetCreationTime());
+		assertTrue(loadedReqIF.getTheHeader().isSetIdentifier());
+		assertFalse(loadedReqIF.getTheHeader().isSetRepositoryId());
+		assertTrue(loadedReqIF.getTheHeader().isSetReqIFToolId());
+		assertTrue(loadedReqIF.getTheHeader().isSetReqIFVersion());
+		assertTrue(loadedReqIF.getTheHeader().isSetSourceToolId());
+		assertTrue(loadedReqIF.getTheHeader().isSetTitle());
+
+		
+		assertEquals(originalReqIF.getTheHeader().getCreationTime().toGregorianCalendar(), loadedReqIF.getTheHeader().getCreationTime().toGregorianCalendar());
+		assertEquals(originalReqIF.getTheHeader().getIdentifier(), loadedReqIF.getTheHeader().getIdentifier());
+		assertEquals(originalReqIF.getTheHeader().getReqIFToolId(), loadedReqIF.getTheHeader().getReqIFToolId());
+		assertEquals(originalReqIF.getTheHeader().getReqIFVersion(), loadedReqIF.getTheHeader().getReqIFVersion());
+		assertEquals(originalReqIF.getTheHeader().getSourceToolId(), loadedReqIF.getTheHeader().getSourceToolId());
+		assertEquals(originalReqIF.getTheHeader().getTitle(), loadedReqIF.getTheHeader().getTitle());
+	}
+	
+	@Test
+	public void testReqIFContent() {
+		assertTrue(loadedReqIF.getCoreContent().eIsSet(ReqIF10Package.eINSTANCE.getReqIFContent_Datatypes()));
+		assertTrue(loadedReqIF.getCoreContent().eIsSet(ReqIF10Package.eINSTANCE.getReqIFContent_Specifications()));
+		assertTrue(loadedReqIF.getCoreContent().eIsSet(ReqIF10Package.eINSTANCE.getReqIFContent_SpecObjects()));
+		assertFalse(loadedReqIF.getCoreContent().eIsSet(ReqIF10Package.eINSTANCE.getReqIFContent_SpecRelationGroups()));
+		assertFalse(loadedReqIF.getCoreContent().eIsSet(ReqIF10Package.eINSTANCE.getReqIFContent_SpecRelations()));
+		assertTrue(loadedReqIF.getCoreContent().eIsSet(ReqIF10Package.eINSTANCE.getReqIFContent_SpecTypes()));
+	}
+	
+	@Test
+	public void testSpecObjectType() {
+		
+		// get the specObject Type
+		ReqIFContent ReqIFContent = loadedReqIF.getCoreContent();
+		assertTrue(ReqIFContent.eIsSet(ReqIF10Package.eINSTANCE.getReqIFContent_SpecTypes()));
+		assertEquals(2 , ReqIFContent.getSpecTypes().size());
+		
+		SpecObjectType specObjectType = null;
+		for (SpecType specType : ReqIFContent.getSpecTypes()) {
+			if (specType instanceof SpecObjectType ) {
+				specObjectType = (SpecObjectType)specType;
+				break;
+			}
+		}
+		
+		assertNotNull("SpecObjectType must be available", specObjectType);
+		
+		// check the specObjectType
+		assertFalse(specObjectType.isSetDesc());
+		assertTrue(specObjectType.isSetIdentifier());
+		assertTrue(specObjectType.isSetLastChange());
+		assertTrue(specObjectType.isSetLongName());
+		assertTrue(specObjectType.eIsSet(ReqIF10Package.eINSTANCE.getSpecType_SpecAttributes()));
+		// TODO: test the values
+	}
+	
+	@Test
+	public void testSpecObject() {
+		
+		// get the specObject Type
+		ReqIFContent ReqIFContent = loadedReqIF.getCoreContent();
+		assertTrue(ReqIFContent.eIsSet(ReqIF10Package.eINSTANCE.getReqIFContent_SpecTypes()));
+		assertEquals(1 , ReqIFContent.getSpecObjects().size());
+		
+		SpecObject specObject = ReqIFContent.getSpecObjects().get(0);
+
+		
+		// check the specObject
+		assertFalse(specObject.isSetDesc());
+		assertTrue(specObject.isSetIdentifier());
+		assertTrue(specObject.isSetLastChange());
+		assertFalse(specObject.isSetLongName());
+		assertTrue(specObject.eIsSet(ReqIF10Package.eINSTANCE.getSpecElementWithAttributes_Values()));
+		assertTrue(specObject.eIsSet(ReqIF10Package.eINSTANCE.getSpecObject_Type()));
+		
+		// check the reference to SpecObjectType
+		SpecObjectType expectedSpecObjectType = null;
+		for (SpecType specType : ReqIFContent.getSpecTypes()) {
+			if (specType instanceof SpecObjectType ) {
+				expectedSpecObjectType = (SpecObjectType)specType;
+				break;
+			}
+		}
+		
+		SpecObjectType specObjectType = specObject.getType();
+		assertNotNull(specObjectType);
+		assertFalse(specObjectType.eIsProxy());
+		assertEquals(expectedSpecObjectType, specObjectType);
+		
+	}
+	
+	
+	
+
+	
+	
+/**
+ * 
+	public void testSchema() throws Throwable {
+		validateSchema(WORKING_DIRECTORY + IPath.SEPARATOR + "TC1000.ReqIF");
+	}
+	*/
+	public static void saveReqIFFile(ReqIF ReqIF, String fileName) throws IOException {
+		URI emfURI = createEMFURI(fileName);
+		ReqIFResourceFactoryImpl resourceFactory = new ReqIFResourceFactoryImpl();
+		ReqIFResourceSetImpl resourceSet = new ReqIFResourceSetImpl(); 
+		Resource resource = resourceFactory.createResource(emfURI); 
+		resourceSet.getResources().add(resource);
+		resource.getContents().add(ReqIF);
+		resource.save(null);
+		resourceSet = null;
+	}
+	
+	public static ReqIF loadReqIFFile(String fileName) throws IOException {
+		
+		URI emfURI = createEMFURI(fileName);
+		ReqIFResourceFactoryImpl resourceFactory = new ReqIFResourceFactoryImpl();
+		XMLResource resource = (XMLResource) resourceFactory.createResource(emfURI);
+
+		resource.load(null);
+		loadedReqIFResource = resource;
+		
+		ReqIFResourceSetImpl resourceSet = new ReqIFResourceSetImpl(); 
+		resourceSet.getResources().add(resource);
+
+        EList<EObject> rootObjects = resource.getContents();
+        System.out.println(rootObjects);
+        if (rootObjects.isEmpty()) {
+        	return null;
+        } else {
+        	return (ReqIF)rootObjects.get(0);
+        }
+	}
+	
+	private static URI createEMFURI(String fileName) {
+		return URI.createURI(WORKING_DIRECTORY + IPath.SEPARATOR + fileName, true);
+	}
+	
+	
+	private void validateAgainstSchema(String filename) throws Exception {
+		
+		StreamSource[] schemaDocuments = new StreamSource[]{new StreamSource("schema/ReqIF.xsd")};
+		//StreamSource[] schemaDocuments = new StreamSource[]{new StreamSource("http://www.omg.org/spec/ReqIF/20110401/ReqIF.xsd")};
+		Source instanceDocument = new StreamSource(WORKING_DIRECTORY + IPath.SEPARATOR + filename);
+
+		SchemaFactory sf = SchemaFactory.newInstance(
+		    "http://www.w3.org/2001/XMLSchema");
+		Schema s = sf.newSchema(schemaDocuments);
+		Validator v = s.newValidator();
+		v.validate(instanceDocument);
+	}
+	
+
+
+}
