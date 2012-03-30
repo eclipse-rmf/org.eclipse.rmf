1d7eedc328bce7819dd562731ad13a4b52fcb0d2
Michael Jastram <michael@jastram.de>
2012-03-30 15:32:47 +0200

    Preparing integration of new Core Conflicts:
    	org.eclipse.rmf.pror.reqif10.edit/plugin.properties

--- org.eclipse.rmf.pror.reqif10.edit/src/org/eclipse/rmf/pror/reqif10/provider/Reqif10EditPlugin.java
+++ org.eclipse.rmf.pror.reqif10.edit/src/org/eclipse/rmf/pror/reqif10/provider/Reqif10EditPlugin.java
@@ -16,7 +16,7 @@ import org.eclipse.emf.common.EMFPlugin;
 import org.eclipse.emf.common.util.ResourceLocator;
 
 /**
- * This is the central singleton for the Reqif10 edit plugin.
+ * This is the central singleton for the Configuration edit plugin.
  * <!-- begin-user-doc -->
  * <!-- end-user-doc -->
  * @generated
