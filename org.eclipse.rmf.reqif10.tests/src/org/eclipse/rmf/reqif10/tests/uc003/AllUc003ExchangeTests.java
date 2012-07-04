8753a58ea08827f55607e63b2241ddcfe1898bf0
Lukas Ladenberger <lukas.ladenberger@googlemail.com>
2012-07-04 10:32:53 +0200

    added test suite for Uc003 exchange tests

---
+++ org.eclipse.rmf.reqif10.tests/src/org/eclipse/rmf/reqif10/tests/uc003/AllUc003ExchangeTests.java
@@ -0,0 +1,12 @@
+package org.eclipse.rmf.reqif10.tests.uc003;
+
+import org.eclipse.rmf.reqif10.tests.uc003.tc18xx.TC18xxHISExchangeProcessTests;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TC18xxHISExchangeProcessTests.class })
+public class AllUc003ExchangeTests {
+
+}
