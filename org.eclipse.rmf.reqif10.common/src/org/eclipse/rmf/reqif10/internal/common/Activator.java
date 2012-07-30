e5e04a8bd3bcd0a9e3e9ad0a1fa5b2724ef37f81
Lukas Ladenberger <lukas.ladenberger@googlemail.com>
2012-07-30 14:47:51 +0200

    added header

--- org.eclipse.rmf.reqif10.common/src/org/eclipse/rmf/reqif10/internal/common/Activator.java
+++ org.eclipse.rmf.reqif10.common/src/org/eclipse/rmf/reqif10/internal/common/Activator.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Formal Mind GmbH and University of Dusseldorf.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Michael Jastram - initial API and implementation
+ *     Lukas Ladenberger - ProR GUI     
+ ******************************************************************************/
 package org.eclipse.rmf.reqif10.internal.common;
 
 import org.osgi.framework.BundleActivator;
