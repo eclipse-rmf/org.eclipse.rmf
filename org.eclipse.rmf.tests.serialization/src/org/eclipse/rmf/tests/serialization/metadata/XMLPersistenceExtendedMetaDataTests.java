3f140f1a70a2e0e38c7e813c78a1d12ddf5002ed
Mark Brörkens <mark.broerkens@itemis.de>
2013-07-16 12:18:48 +0200

    ASSIGNED - bug 376942: Third-Party Tool Extensions not handled correctly https://bugs.eclipse.org/bugs/show_bug.cgi?id=376942
    continued renaming

--- org.eclipse.rmf.tests.serialization/src/org/eclipse/rmf/tests/serialization/metadata/XMLPersistenceExtendedMetaDataTests.java
+++ org.eclipse.rmf.tests.serialization/src/org/eclipse/rmf/tests/serialization/metadata/XMLPersistenceExtendedMetaDataTests.java
@@ -25,7 +25,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 @SuppressWarnings("nls")
-public class RMFExtendedMetaDataTests {
+public class XMLPersistenceExtendedMetaDataTests {
 	int[] identitySerializationStructureConfiguration = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }; // identity
 																													// mapping
 
