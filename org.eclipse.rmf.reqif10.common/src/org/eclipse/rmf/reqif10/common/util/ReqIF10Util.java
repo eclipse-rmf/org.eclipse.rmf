9a12fdced2260490e5ed9e061123503652fd2811
François Rey <eclipse.org@francois.rey.name>
2013-07-18 00:12:25 +0200

    Better use of EMF reflective API in the ReqIF10Util class.
    Many methods in ReqIF10Util are there to provide polymorphism over ReqIF
    classes that define the same feature in leaf classes. For example the
    SpecElementWithAttributes descendants, Specification, SpecObject, and
    SpecRelation, all define a type attribute, but there is no way to handle
    it polymorphically at the level of SpecElementWithAttributes. Same for
    the AttributeDefinition descendants and default values. This change
    makes better use of EMF reflective API, at times replacing lots of
    identical code blocks handling all descendants. Generated code has been
    favored in order to raise compiler errors in case of model changes.
    
    
    Change-Id: I406023c3871cf1cfdf6c9481cca0753a4070b34e
    Signed-off-by: François Rey <eclipse.org@francois.rey.name>

--- org.eclipse.rmf.reqif10.common/src/org/eclipse/rmf/reqif10/common/util/ReqIF10Util.java
+++ org.eclipse.rmf.reqif10.common/src/org/eclipse/rmf/reqif10/common/util/ReqIF10Util.java
@@ -7,25 +7,26 @@
  * 
  * Contributors:
  *     Nirmal Sasidharan - initial API and implementation
+ *     François Rey - better use of EMF reflective API
  ******************************************************************************/
 package org.eclipse.rmf.reqif10.common.util;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.Collection;
 import java.util.Date;
 import java.util.GregorianCalendar;
-import java.util.List;
 
 import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeConstants;
 import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
 
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.impl.ResourceImpl;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.rmf.reqif10.AttributeDefinition;
 import org.eclipse.rmf.reqif10.AttributeDefinitionBoolean;
 import org.eclipse.rmf.reqif10.AttributeDefinitionDate;
@@ -40,14 +41,12 @@ import org.eclipse.rmf.reqif10.AttributeValueDate;
 import org.eclipse.rmf.reqif10.AttributeValueEnumeration;
 import org.eclipse.rmf.reqif10.AttributeValueInteger;
 import org.eclipse.rmf.reqif10.AttributeValueReal;
-import org.eclipse.rmf.reqif10.AttributeValueSimple;
 import org.eclipse.rmf.reqif10.AttributeValueString;
 import org.eclipse.rmf.reqif10.AttributeValueXHTML;
 import org.eclipse.rmf.reqif10.DatatypeDefinition;
 import org.eclipse.rmf.reqif10.EnumValue;
 import org.eclipse.rmf.reqif10.Identifiable;
 import org.eclipse.rmf.reqif10.ReqIF;
-import org.eclipse.rmf.reqif10.ReqIF10Factory;
 import org.eclipse.rmf.reqif10.ReqIF10Package;
 import org.eclipse.rmf.reqif10.SpecElementWithAttributes;
 import org.eclipse.rmf.reqif10.SpecType;
@@ -96,32 +95,24 @@ public class ReqIF10Util {
 	 * method. This convenience method returns the value.
 	 */
 	public static Object getTheValue(AttributeValue attributeValue) {
-		if (attributeValue instanceof AttributeValueBoolean) {
-			return ((AttributeValueBoolean) attributeValue).isTheValue();
-		} else if (attributeValue instanceof AttributeValueSimple || attributeValue instanceof AttributeValueXHTML) {
-			return reflectiveGet(attributeValue, "getTheValue"); //$NON-NLS-1$
-		} else if (attributeValue instanceof AttributeValueEnumeration) {
-			return reflectiveGet(attributeValue, "getValues"); //$NON-NLS-1$
-		} else {
-			throw new IllegalArgumentException("Can't get value from " + attributeValue); //$NON-NLS-1$
-		}
+		EStructuralFeature theValueFeature = getTheValueFeature(attributeValue);
+		return attributeValue.eGet(theValueFeature);
 	}
 
 	/**
-	 * Reflectively sets the value. The value must not be null, as it is used to infer the class.
+	 * Reflectively sets the value.
 	 * 
 	 * @param attributeValue
 	 */
 	@SuppressWarnings("unchecked")
-	// for AttributeValueEnumeration
 	public static void setTheValue(AttributeValue attributeValue, Object value) {
-		if (attributeValue instanceof AttributeValueSimple || attributeValue instanceof AttributeValueXHTML) {
-			reflectiveSet(attributeValue, value, "setTheValue"); //$NON-NLS-1$
-		} else if (attributeValue instanceof AttributeValueEnumeration) {
-			((AttributeValueEnumeration) attributeValue).getValues().clear();
-			((AttributeValueEnumeration) attributeValue).getValues().addAll((Collection<? extends EnumValue>) value);
+		if (attributeValue instanceof AttributeValueEnumeration) {
+			EList<EnumValue> enumValues = ((AttributeValueEnumeration) attributeValue).getValues();
+			enumValues.clear();
+			enumValues.addAll((Collection<? extends EnumValue>) value);
 		} else {
-			throw new IllegalArgumentException("Can't get value from " + attributeValue); //$NON-NLS-1$
+			EStructuralFeature theValueFeature = getTheValueFeature(attributeValue);
+			attributeValue.eSet(theValueFeature, value);
 		}
 	}
 
@@ -130,7 +121,7 @@ public class ReqIF10Util {
 	 */
 	public static AttributeValue getAttributeValue(SpecElementWithAttributes specElement, AttributeDefinition attributeDefinition) {
 		for (AttributeValue value : specElement.getValues()) {
-			AttributeDefinition definition = (AttributeDefinition) reflectiveGet(value, "getDefinition"); //$NON-NLS-1$
+			AttributeDefinition definition = getAttributeDefinition(value);
 			if (attributeDefinition.equals(definition)) {
 				return value;
 			}
@@ -140,8 +131,8 @@ public class ReqIF10Util {
 
 	/**
 	 * Finds the {@link AttributeValue} for the given {@link SpecElementWithUserDefinedAttributes}. If it does not exist
-	 * yet, it is created (but not attached to the specElement. If an attributeDefinition with the label does not exist,
-	 * null is returned.
+	 * yet, it is created (but not attached to the specElement). If an attributeDefinition with the label does not
+	 * exist, null is returned.
 	 * <p>
 	 * If a default value is available, it is set as well.
 	 */
@@ -168,31 +159,17 @@ public class ReqIF10Util {
 			return null;
 		}
 
-		// Is there already a value with this AttributeDefinition?
-		for (AttributeValue value : element.getValues()) {
-			AttributeDefinition ad = getAttributeDefinition(value);
-			if (attrDef.equals(ad)) {
-				return value;
-			}
-		}
-
-		// No: Create a new AttributeDefinition
-		AttributeValue av = createAttributeValue(attrDef);
-		AttributeValue defaultValue = (AttributeValue) ReqIF10Util.reflectiveGet(attrDef, "getDefaultValue"); //$NON-NLS-1$
-		if (defaultValue != null) {
-			Object v = ReqIF10Util.getTheValue(defaultValue);
-			if (v != null) {
-				ReqIF10Util.setTheValue(av, v);
-			}
-		}
-		return av;
+		// return existing value or a new one
+		AttributeValue av = getAttributeValue(element, attrDef);
+		return av != null ? av : createAttributeValue(attrDef);
 	}
 
 	/**
-	 * Returns the AttributeDefinition for a given value (Would be so much easier with inheritance).
+	 * Returns the AttributeDefinition for a given value.
 	 */
-	public static AttributeDefinition getAttributeDefinition(AttributeValue value) {
-		return (AttributeDefinition) reflectiveGet(value, "getDefinition"); //$NON-NLS-1$
+	public static AttributeDefinition getAttributeDefinition(AttributeValue attributeValue) {
+		EStructuralFeature theDefinitionFeature = getDefinitionFeature(attributeValue);
+		return (AttributeDefinition) attributeValue.eGet(theDefinitionFeature);
 	}
 
 	/**
@@ -200,7 +177,10 @@ public class ReqIF10Util {
 	 * inheritance).
 	 */
 	public static DatatypeDefinition getDatatypeDefinition(AttributeDefinition attributeDefinition) {
-		return (DatatypeDefinition) reflectiveGet(attributeDefinition, "getType"); //$NON-NLS-1$
+		// Using generated code instead of literal value "type"
+		// A model change will should raise a compiler error
+		String featureName = ReqIF10Package.eINSTANCE.getAttributeDefinitionString_Type().getName();
+		return (DatatypeDefinition) reflectiveGet(attributeDefinition, featureName);
 	}
 
 	/**
@@ -220,7 +200,10 @@ public class ReqIF10Util {
 	}
 
 	public static SpecType getSpecType(SpecElementWithAttributes specElement) {
-		return (SpecType) reflectiveGet(specElement, "getType"); //$NON-NLS-1$
+		// Using generated code instead of literal value "type"
+		// A model change will should raise a compiler error
+		String featureName = ReqIF10Package.eINSTANCE.getSpecObject_Type().getName();
+		return (SpecType) reflectiveGet(specElement, featureName);
 	}
 
 	public static SpecType getSpecType(AttributeDefinition ad) {
@@ -228,53 +211,55 @@ public class ReqIF10Util {
 	}
 
 	/**
-	 * Helper method that reflectively executes methods. The annoying thing with ReqIF is, that many classes share a
-	 * method (e.g. getType()) that returns the same supertype, but due to the model generation, there isn't a shared
-	 * method in the superclass. If there is a problem, the resulting {@link Exception} is wrapped into a
-	 * {@link RuntimeException}.
-	 * 
-	 * @param object
-	 * @param methodName
-	 * @return the result of the method call
-	 * @throws RuntimeException
+	 * Helper method that uses EMF reflective API to retrieve a feature given its name.
 	 */
-	public static Object reflectiveGet(Object object, String methodName) {
-		try {
-			Method method = object.getClass().getMethod(methodName, (Class<?>[]) null);
-			return method.invoke(object, (Object[]) null);
-		} catch (SecurityException e) {
-			throw new RuntimeException(e);
-		} catch (NoSuchMethodException e) {
-			throw new RuntimeException(e);
-		} catch (IllegalArgumentException e) {
-			throw new RuntimeException(e);
-		} catch (IllegalAccessException e) {
-			throw new RuntimeException(e);
-		} catch (InvocationTargetException e) {
-			throw new RuntimeException(e);
-		}
+	public static EStructuralFeature getFeature(EObject object, String featureName) {
+		return object.eClass().getEStructuralFeature(featureName);
 	}
 
-	public static void reflectiveSet(Object object, Object value, String methodName) {
-		try {
-			Class<?>[] args = new Class<?>[1];
-			if (value instanceof List) {
-				args[0] = List.class;
-			} else {
-				args[0] = value.getClass();
-			}
-			Method method = object.getClass().getMethod(methodName, args);
-			method.invoke(object, value);
-		} catch (SecurityException e) {
-			throw new RuntimeException(e);
-		} catch (NoSuchMethodException e) {
-			throw new RuntimeException(e);
-		} catch (IllegalArgumentException e) {
-			throw new RuntimeException(e);
-		} catch (IllegalAccessException e) {
-			throw new RuntimeException(e);
-		} catch (InvocationTargetException e) {
-			throw new RuntimeException(e);
+	/**
+	 * Helper method that uses EMF reflective API to retrieve the value for a given feature name.
+	 */
+	public static Object reflectiveGet(EObject object, String featureName) {
+		return object.eGet(getFeature(object, featureName));
+	}
+
+	/**
+	 * Helper method that uses EMF reflective API to set the value for a given feature name.
+	 */
+	public static void reflectiveSet(EObject object, String featureName, Object value) {
+		object.eSet(getFeature(object, featureName), value);
+	}
+
+	/**
+	 * Returns the "the value" feature for the given attributeValue. For instance, for an {@link AttributeValueString}
+	 * it returns {@link Reqif10Package.Literals#ATTRIBUTE_VALUE_STRING__THE_VALUE}. The one exception is
+	 * {@link AttributeValueEnumeration}, where the feature name is "values", rather than "the value".
+	 * 
+	 * @throws IllegalArgumentException
+	 *             for unknown {@link AttributeValue}s.
+	 */
+	public static EStructuralFeature getDefinitionFeature(AttributeValue attributeValue) {
+		// This implementation may be tedious to write but has the advantage
+		// of raising compiler errors if the model changes.
+		// The much shorter and reflective version would not:
+		//return getFeature(attributeValue, "definition"); //$NON-NLS-1$
+		if (attributeValue instanceof AttributeValueBoolean) {
+			return ReqIF10Package.Literals.ATTRIBUTE_VALUE_BOOLEAN__DEFINITION;
+		} else if (attributeValue instanceof AttributeValueDate) {
+			return ReqIF10Package.Literals.ATTRIBUTE_VALUE_DATE__DEFINITION;
+		} else if (attributeValue instanceof AttributeValueInteger) {
+			return ReqIF10Package.Literals.ATTRIBUTE_VALUE_INTEGER__DEFINITION;
+		} else if (attributeValue instanceof AttributeValueReal) {
+			return ReqIF10Package.Literals.ATTRIBUTE_VALUE_REAL__DEFINITION;
+		} else if (attributeValue instanceof AttributeValueString) {
+			return ReqIF10Package.Literals.ATTRIBUTE_VALUE_STRING__DEFINITION;
+		} else if (attributeValue instanceof AttributeValueXHTML) {
+			return ReqIF10Package.Literals.ATTRIBUTE_VALUE_XHTML__DEFINITION;
+		} else if (attributeValue instanceof AttributeValueEnumeration) {
+			return ReqIF10Package.Literals.ATTRIBUTE_VALUE_ENUMERATION__DEFINITION;
+		} else {
+			throw new IllegalArgumentException("Unknown AttributeValue: " + attributeValue); //$NON-NLS-1$
 		}
 	}
 
@@ -287,6 +272,10 @@ public class ReqIF10Util {
 	 *             for unknown {@link AttributeValue}s.
 	 */
 	public static EStructuralFeature getTheValueFeature(AttributeValue attributeValue) {
+		// This implementation may be tedious to write but has the advantage
+		// of raising compiler errors if the model changes.
+		// The much shorter and reflective version would not:
+		//return getFeature(attributeValue, attributeValue instanceof AttributeValueEnumeration ? "values" : "theValue"); //$NON-NLS-1$
 		if (attributeValue instanceof AttributeValueBoolean) {
 			return ReqIF10Package.Literals.ATTRIBUTE_VALUE_BOOLEAN__THE_VALUE;
 		} else if (attributeValue instanceof AttributeValueDate) {
@@ -307,73 +296,57 @@ public class ReqIF10Util {
 	}
 
 	/**
-	 * Returns an empty value of the correct type for the given {@link AttributeDefinition} (Would be so much easier
-	 * with inheritance). Note that we do not use the command stack here.
-	 * <p>
-	 * TODO There must be a better way (reflection?)
+	 * Returns the "defaultValue" feature for the given attributeDefinition. For instance, for an
+	 * {@link AttributeDefinitionString} it returns
+	 * {@link Reqif10Package.Literals#ATTRIBUTE_DEFINITION_STRING__DEFAULT_VALUE}.
+	 * 
+	 * @throws IllegalArgumentException
+	 *             for an unknown {@link AttributeDefinition}.
+	 */
+	public static EStructuralFeature getDefaultValueFeature(AttributeDefinition attributeDefinition) {
+		// This implementation may be tedious to write but has the advantage
+		// of raising compiler errors if the model changes.
+		// The much shorter and reflective version would not:
+		//return getFeature(attributeDefinition, "defaultValue"); //$NON-NLS-1$
+		if (attributeDefinition instanceof AttributeDefinitionBoolean) {
+			return ReqIF10Package.Literals.ATTRIBUTE_DEFINITION_BOOLEAN__DEFAULT_VALUE;
+		} else if (attributeDefinition instanceof AttributeDefinitionDate) {
+			return ReqIF10Package.Literals.ATTRIBUTE_DEFINITION_DATE__DEFAULT_VALUE;
+		} else if (attributeDefinition instanceof AttributeDefinitionInteger) {
+			return ReqIF10Package.Literals.ATTRIBUTE_DEFINITION_INTEGER__DEFAULT_VALUE;
+		} else if (attributeDefinition instanceof AttributeDefinitionReal) {
+			return ReqIF10Package.Literals.ATTRIBUTE_DEFINITION_REAL__DEFAULT_VALUE;
+		} else if (attributeDefinition instanceof AttributeDefinitionString) {
+			return ReqIF10Package.Literals.ATTRIBUTE_DEFINITION_STRING__DEFAULT_VALUE;
+		} else if (attributeDefinition instanceof AttributeDefinitionXHTML) {
+			return ReqIF10Package.Literals.ATTRIBUTE_DEFINITION_XHTML__DEFAULT_VALUE;
+		} else if (attributeDefinition instanceof AttributeDefinitionEnumeration) {
+			return ReqIF10Package.Literals.ATTRIBUTE_DEFINITION_ENUMERATION__DEFAULT_VALUE;
+		} else {
+			throw new IllegalArgumentException("Unknown AttributeDefinition: " + attributeDefinition); //$NON-NLS-1$
+		}
+	}
+
+	/**
+	 * Returns an empty value of the correct type for the given {@link AttributeDefinition}. Note that we do not use the
+	 * command stack here.
 	 */
 	public static AttributeValue createAttributeValue(AttributeDefinition attributeDefinition) {
 		if (attributeDefinition == null) {
 			return null;
-		} else if (attributeDefinition instanceof AttributeDefinitionBoolean) {
-			AttributeValueBoolean value = ReqIF10Factory.eINSTANCE.createAttributeValueBoolean();
-			value.setDefinition((AttributeDefinitionBoolean) attributeDefinition);
-			AttributeValueBoolean defaultValue = ((AttributeDefinitionBoolean) attributeDefinition).getDefaultValue();
-			if (defaultValue != null) {
-				value.setTheValue(defaultValue.isTheValue());
-			}
-			return value;
-		} else if (attributeDefinition instanceof AttributeDefinitionDate) {
-			AttributeValueDate value = ReqIF10Factory.eINSTANCE.createAttributeValueDate();
-			value.setDefinition((AttributeDefinitionDate) attributeDefinition);
-			AttributeValueDate defaultValue = ((AttributeDefinitionDate) attributeDefinition).getDefaultValue();
-			if (defaultValue != null) {
-				value.setTheValue(defaultValue.getTheValue());
-			}
-			return value;
-		} else if (attributeDefinition instanceof AttributeDefinitionInteger) {
-			AttributeValueInteger value = ReqIF10Factory.eINSTANCE.createAttributeValueInteger();
-			value.setDefinition((AttributeDefinitionInteger) attributeDefinition);
-			AttributeValueInteger defaultValue = ((AttributeDefinitionInteger) attributeDefinition).getDefaultValue();
-			if (defaultValue != null) {
-				value.setTheValue(defaultValue.getTheValue());
-			}
-			return value;
-		} else if (attributeDefinition instanceof AttributeDefinitionReal) {
-			AttributeValueReal value = ReqIF10Factory.eINSTANCE.createAttributeValueReal();
-			value.setDefinition((AttributeDefinitionReal) attributeDefinition);
-			AttributeValueReal defaultValue = ((AttributeDefinitionReal) attributeDefinition).getDefaultValue();
-			if (defaultValue != null) {
-				value.setTheValue(defaultValue.getTheValue());
-			}
-			return value;
-		} else if (attributeDefinition instanceof AttributeDefinitionString) {
-			AttributeValueString value = ReqIF10Factory.eINSTANCE.createAttributeValueString();
-			value.setDefinition((AttributeDefinitionString) attributeDefinition);
-
-			AttributeValueString defaultValue = ((AttributeDefinitionString) attributeDefinition).getDefaultValue();
-			if (defaultValue != null) {
-				value.setTheValue(defaultValue.getTheValue());
-			}
-			return value;
-		} else if (attributeDefinition instanceof AttributeDefinitionXHTML) {
-			AttributeValueXHTML value = ReqIF10Factory.eINSTANCE.createAttributeValueXHTML();
-			value.setDefinition((AttributeDefinitionXHTML) attributeDefinition);
-			AttributeValueXHTML defaultValue = ((AttributeDefinitionXHTML) attributeDefinition).getDefaultValue();
-			if (defaultValue != null) {
-				value.setTheValue(defaultValue.getTheValue());
-			}
-			return value;
-		} else if (attributeDefinition instanceof AttributeDefinitionEnumeration) {
-			AttributeValueEnumeration value = ReqIF10Factory.eINSTANCE.createAttributeValueEnumeration();
-			value.setDefinition((AttributeDefinitionEnumeration) attributeDefinition);
-			AttributeValueEnumeration defaultValue = ((AttributeDefinitionEnumeration) attributeDefinition).getDefaultValue();
-			if (defaultValue != null) {
-				value.getValues().addAll(defaultValue.getValues());
-			}
-			return value;
 		} else {
-			throw new IllegalArgumentException("Type not supported: " + attributeDefinition); //$NON-NLS-1$
+			// Get the default value class, and instantiate it
+			EStructuralFeature defaultValueFeature = getDefaultValueFeature(attributeDefinition);
+			EClass attributeValueClass = (EClass) defaultValueFeature.getEType();
+			AttributeValue value = (AttributeValue) EcoreUtil.create(attributeValueClass);
+			// Set the link back to the definition object
+			value.eSet(getDefinitionFeature(value), attributeDefinition);
+			// Set the value to the default value if any
+			AttributeValue defaultValue = (AttributeValue) attributeDefinition.eGet(defaultValueFeature);
+			if (defaultValue != null) {
+				setTheValue(value, getTheValue(defaultValue));
+			}
+			return value;
 		}
 	}
 
