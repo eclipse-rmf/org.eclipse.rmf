46731929780a2edbf4c9ad8a2ef42d7d459b428b
Michael Jastram <michael@jastram.de>
2014-07-18 14:38:48 +0200

    RESOLVED - bug 439887: Provide Custom Column Header for standard attributes https://bugs.eclipse.org/bugs/show_bug.cgi?id=439887

--- org.eclipse.rmf.reqif10.pror.editor/src/org/eclipse/rmf/reqif10/pror/editor/agilegrid/ProrCellRendererProvider.java
+++ org.eclipse.rmf.reqif10.pror.editor/src/org/eclipse/rmf/reqif10/pror/editor/agilegrid/ProrCellRendererProvider.java
@@ -13,25 +13,26 @@ package org.eclipse.rmf.reqif10.pror.editor.agilegrid;
 import org.agilemore.agilegrid.AgileGrid;
 import org.agilemore.agilegrid.DefaultCellRendererProvider;
 import org.agilemore.agilegrid.ICellRenderer;
+import org.agilemore.agilegrid.ILayoutAdvisor;
+import org.agilemore.agilegrid.SWTResourceManager;
 import org.agilemore.agilegrid.renderers.HeaderCellRenderer;
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Rectangle;
 
 public class ProrCellRendererProvider extends DefaultCellRendererProvider {
 
 	private final ProrCellRenderer prorCellRenderer;
-	private final ICellRenderer leftHeadRenderer;
+	private final ICellRenderer headRenderer;
 	private final ProrLinkCellRenderer prorLinkCellRenderer;
 
 	public ProrCellRendererProvider(AgileGrid agileGrid, AdapterFactory adapterFactory, EditingDomain editingDomain) {
 		super(agileGrid);
 		prorCellRenderer = new ProrCellRenderer(agileGrid, adapterFactory, editingDomain);
-		leftHeadRenderer = new HeaderCellRenderer(agileGrid,
-				ICellRenderer.STYLE_FLAT
-						| ICellRenderer.INDICATION_SELECTION_ROW);
-		leftHeadRenderer.setAlignment(ICellRenderer.ALIGN_HORIZONTAL_LEFT
-				| ICellRenderer.ALIGN_VERTICAL_CENTER);
-
+		headRenderer = new ProrHeaderCellRenderer(agileGrid);
 		prorLinkCellRenderer = new ProrLinkCellRenderer(agileGrid,
 				adapterFactory);
 	}
@@ -47,6 +48,61 @@ public class ProrCellRendererProvider extends DefaultCellRendererProvider {
 
 	@Override
 	public ICellRenderer getLeftHeadRenderer(int row) {
-		return leftHeadRenderer;
+		return headRenderer;
 	}
+	
+	@Override
+	public ICellRenderer getTopHeadRenderer(int row) {
+		return headRenderer;
+	}
+}
+
+/**
+ * A Renderer that strips off the "ReqIF." from standard attributes and
+ * changes the label color.
+ */
+class ProrHeaderCellRenderer extends HeaderCellRenderer {
+
+	public static Color COLOR_REQIF_ATTRIBUTE = SWTResourceManager
+			.getColor(SWT.COLOR_BLUE);
+	
+	public ProrHeaderCellRenderer(AgileGrid agileGrid) {
+		super(agileGrid, ICellRenderer.STYLE_FLAT
+				| ICellRenderer.INDICATION_SELECTION_ROW);
+		setAlignment(ICellRenderer.ALIGN_HORIZONTAL_LEFT
+		| ICellRenderer.ALIGN_VERTICAL_CENTER);
+	}
+	
+	/**
+	 * Taken from {@link HeaderCellRenderer#doDrawCellContent}, but with
+	 * special handling of labels starting with "ReqIF."
+	 */
+	@Override
+	protected void doDrawCellContent(GC gc, Rectangle rect, int row, int col) {
+		// draw text and image in the given area.
+		String label = "";
+		ILayoutAdvisor layoutAdvisor = agileGrid.getLayoutAdvisor();
+		if (row == -1 && col >= 0 && col < layoutAdvisor.getColumnCount()) {
+			label = layoutAdvisor.getTopHeaderLabel(col);
+		} else if (col == -1 && row >= 0 && row < layoutAdvisor.getRowCount()) {
+			label = layoutAdvisor.getLeftHeaderLabel(row);
+		} else if (row == -1 && col == -1) {
+			label = "";
+		}
+
+		// Handling of ReqIF. Attributes
+		if (label.startsWith("ReqIF.")) {
+			label = label.substring(6);
+			gc.setForeground(COLOR_REQIF_ATTRIBUTE);
+		}
+
+		int alignment = getAlignment();
+		drawTextImage(gc, label, alignment, null, alignment, rect.x + 3,
+				rect.y + 2, rect.width - 6, rect.height - 4);
+
+		drawSortIndicator(gc, rect, row, col, label);
+	}
+	
+	
+	
 }
