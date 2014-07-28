1d0e121b825f87b9586acd337ecfc0ee37829e01
Hussein MHANNA <hussein.mhanna@all4tec.net>
2014-07-29 00:18:35 +0200

    This fix the bug #440511.
    The problem was that the IDENTFIER feature are stored in UpperCase in
    the reqif file but in the XMLMap we have put it as LowerCase.
    
    Conflicts, resolved by Michael Jastram <michael@jastram.de>:
    	org.eclipse.rmf.reqif10.serialization/src/org/eclipse/rmf/reqif10/serialization/ReqIF10ResourceImpl.java
    
    Change-Id: I452ba58c92a2fddf2726a2ee45a5a11f39daca85
    Signed-off-by: Hussein MHANNA <hussein.mhanna@all4tec.net>

--- org.eclipse.rmf.reqif10.serialization/src/org/eclipse/rmf/reqif10/serialization/ReqIF10ResourceImpl.java
+++ org.eclipse.rmf.reqif10.serialization/src/org/eclipse/rmf/reqif10/serialization/ReqIF10ResourceImpl.java
@@ -16,8 +16,13 @@ import java.util.HashSet;
 import java.util.Map;
 
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.eclipse.emf.ecore.xmi.XMLSave;
+import org.eclipse.emf.ecore.xmi.impl.XMLMapImpl;
+import org.eclipse.rmf.internal.serialization.XMLPersistenceMappingSaveImpl;
 import org.eclipse.rmf.reqif10.ReqIF10Package;
 import org.eclipse.rmf.reqif10.xhtml.XhtmlPackage;
 import org.eclipse.rmf.serialization.IdAdapter;
@@ -49,6 +54,12 @@ public class ReqIF10ResourceImpl extends XMLPersistenceMappingResourceImpl {
 	@Override
 	public void initDefaultOptions() {
 		super.initDefaultOptions();
+		// ========= create options map===================
+		final XMLResource.XMLMap optionsMap = new XMLMapImpl();
+		optionsMap
+				.setIDAttributeName(ReqIF10Package.Literals.IDENTIFIABLE__IDENTIFIER
+						.getName().toUpperCase());
+
 		// ========= default save options ===================
 		Map<Object, Object> saveOptions = getDefaultSaveOptions();
 		Map<String, String> namespaceToPrefixMap = new HashMap<String, String>();
@@ -58,7 +69,74 @@ public class ReqIF10ResourceImpl extends XMLPersistenceMappingResourceImpl {
 		
 		// ========= default load options ===================
 		Map<Object, Object> loadOptions = getDefaultLoadOptions();
-
+		loadOptions.put(XMLResource.OPTION_XML_MAP, optionsMap);
 	}
 
+	/**
+	 * Return <code>true</code>.
+	 * 
+	 * @return <code>true</code>.
+	 * 
+	 * @see org.eclipse.emf.ecore.xmi.impl.XMLResourceImpl#useUUIDs()
+	 */
+	@Override
+	protected boolean useUUIDs() {
+		return true;
+	}
+
+	/**
+	 * Return <code>false</code>.
+	 * 
+	 * @return <code>false</code>.
+	 * 
+	 * @see org.eclipse.emf.ecore.xmi.impl.XMLResourceImpl#assignIDsWhileLoading()
+	 */
+	@Override
+	protected boolean assignIDsWhileLoading() {
+		return false;
+	}
+
+	/**
+	 * Sets the ID of the object. The default implementation will update the
+	 * {@link #eObjectToIDMap}. This behavior is override to set the ID in a
+	 * object's specific attribute to set the id in the
+	 * {@link Identifiable#setIdentifier(String)} and call the super method.
+	 * 
+	 * @param eObject
+	 *            : The object where the Id must be set.
+	 * @param id
+	 *            : The object's Id.
+	 * @see org.eclipse.emf.ecore.xmi.impl.XMLResourceImpl#setID(org.eclipse.emf.ecore.EObject,
+	 *      java.lang.String)
+	 */
+	@Override
+	public void setID(final EObject eObject, final String id) {
+		final EAttribute idAttribute = eObject.eClass().getEIDAttribute();
+		if ((idAttribute != null) && (id != null)) {
+			eObject.eSet(idAttribute, id);
+		}
+		super.setID(eObject, id);
+	}
+
+	/**
+	 * Create a new XMLSave implementation, with our specific implementation to
+	 * avoid the save of the id of an {@link EObject} twice via the
+	 * {@link Identifiable#getIdentifier()} and the #idToEObjectMap.
+	 * 
+	 * @return The XMLSave created
+	 * 
+	 * @see org.eclipse.emf.ecore.xmi.impl.XMLResourceImpl#createXMLSave()
+	 */
+	@Override
+	protected XMLSave createXMLSave() {
+		return new XMLPersistenceMappingSaveImpl(createXMLHelper()) {
+
+			@Override
+			protected void saveElementID(final EObject o) {
+				// As the identifier is an EStructuralFeture of the Identifiable
+				// class, so we ignore the save of the element id from the map.
+				this.saveFeatures(o);
+			}
+		};
+	}
 }
