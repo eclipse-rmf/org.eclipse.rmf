9d2ac5e8c2c391d801a2c25c4d0502c12cebd979
Michael Jastram <michael@jastram.de>
2014-10-14 10:36:45 +0200

    Eclipse Compliance: Adding Headers notice and epl files.

--- org.eclipse.rmf.reqif10.csv.importer.edit/src/org/eclipse/rmf/reqif10/csv/importer/mapping/provider/MappingLibraryItemProvider.java
+++ org.eclipse.rmf.reqif10.csv.importer.edit/src/org/eclipse/rmf/reqif10/csv/importer/mapping/provider/MappingLibraryItemProvider.java
@@ -1,5 +1,14 @@
-/**
- */
+/*******************************************************************************
+ * Copyright (c) 2014 Hussein Mhanna
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Hussein Mhanna - initial API and implementation
+ ******************************************************************************/
 package org.eclipse.rmf.reqif10.csv.importer.mapping.provider;
 
 import java.util.Collection;
