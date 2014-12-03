a6254849382b5b9c1882e00d6a6561cc550087ba
Michael Jastram <michael@jastram.de>
2014-12-03 12:51:23 +0100

    Label Support for Filter Operations.

--- org.eclipse.rmf.reqif10.search/src/org/eclipse/rmf/reqif10/search/filter/IFilter.java
+++ org.eclipse.rmf.reqif10.search/src/org/eclipse/rmf/reqif10/search/filter/IFilter.java
@@ -19,23 +19,33 @@ import com.google.common.collect.ImmutableSet;
 public interface IFilter extends ReqifFilter {
 
 	public enum Operator{
-		EQUALS,
-		IS,
-		IS_NOT,
-		NOT_EQUALS,
-		CONTAINS,
-		NOT_CONTAINS,
-		REGEXP,
-		REGEXP_PLAIN,
-		BETWEEN,
-		GREATER,
-		SMALLER,
-		BEFORE,
-		AFTER,
-		CONTAINS_ALL,
-		CONTAINS_ANY
+		EQUALS("operator_equals"),
+		IS("operator_is"),
+		IS_NOT("operator_is_not"),
+		NOT_EQUALS("operator_not_equals"),
+		CONTAINS("operator_contains"),
+		NOT_CONTAINS("operator_not_contains"),
+		REGEXP("operator_regexp"),
+		REGEXP_PLAIN("operator_regexp_plain"),
+		BETWEEN("operator_between"),
+		GREATER("operator_greater"),
+		SMALLER("operator_smaller"),
+		BEFORE("operator_before"),
+		AFTER("operator_after"),
+		CONTAINS_ALL("operator_contains_all"),
+		CONTAINS_ANY("operator_contains_any");
+		
+		private String label;
+		private Operator(String label) {
+			this.label = label;
+		}
+		@Override
+		public String toString() {
+			// TODO NS-Lookup
+			return label;
+		}
 	}
-
+	
 	/**
 	 * @return The given Operations supported by a concrete {@link IFilter} implementation.
 	 */
