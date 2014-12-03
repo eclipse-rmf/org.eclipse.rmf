af301c7a80a42833ce5ff15cbfc6a65701fee6a5
Ingo Weigelt <iw@wglt.de>
2014-12-03 17:19:35 +0100

    copyright notice

--- org.eclipse.rmf.reqif10.search.test/src/org/eclipse/rmf/reqif10/search/test/StringFilterTest.java
+++ org.eclipse.rmf.reqif10.search.test/src/org/eclipse/rmf/reqif10/search/test/StringFilterTest.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Formal Mind GmbH.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Ingo Weigelt - initial API and implementation
+ *     Michael Jastram - adding SUPPORTED_OPERATIONS
+ ******************************************************************************/
 package org.eclipse.rmf.reqif10.search.test;
 
 import static org.junit.Assert.*;
