76cda2660ef8b4f7880e48ccdf2b6fe23eea19fc
Michael Jastram <michael@jastram.de>
2014-12-08 13:49:57 +0100

    Implemented Date-Time-Picker for DateTime

--- org.eclipse.rmf.reqif10.pror.editor/src/org/eclipse/rmf/reqif10/pror/editor/agilegrid/ProrDateCellEditor.java
+++ org.eclipse.rmf.reqif10.pror.editor/src/org/eclipse/rmf/reqif10/pror/editor/agilegrid/ProrDateCellEditor.java
@@ -10,69 +10,104 @@
  ******************************************************************************/
 package org.eclipse.rmf.reqif10.pror.editor.agilegrid;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.util.Date;
+import java.util.Calendar;
 import java.util.GregorianCalendar;
 
-import javax.xml.datatype.DatatypeConfigurationException;
-
 import org.agilemore.agilegrid.AgileGrid;
-import org.agilemore.agilegrid.ICellEditorValidator;
+import org.agilemore.agilegrid.editors.PopupCellEditor;
 import org.eclipse.emf.edit.domain.EditingDomain;
-import org.eclipse.rmf.reqif10.AttributeValue;
+import org.eclipse.rmf.reqif10.AttributeValueDate;
 import org.eclipse.rmf.reqif10.common.util.ReqIF10Util;
 import org.eclipse.rmf.reqif10.pror.util.ProrUtil;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.DateTime;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 
-public class ProrDateCellEditor extends ProrCellEditor {
+public class ProrDateCellEditor extends PopupCellEditor {
+
+	private DateTime date;
+	private DateTime time;
+	private AttributeValueDate attributeValue;
+	private EditingDomain editingDomain;
+	private Object affectedObject;
+	private Object parent;
 
 	public ProrDateCellEditor(AgileGrid agileGrid, EditingDomain editingDomain,
 			Object parent,
 			Object affectedObject) {
-		super(agileGrid, editingDomain, parent, affectedObject);
-		this.setValidator(new ICellEditorValidator() {
-			public String isValid(Object value) {
-				if (value == null) {
-					return null;
-				}
-				try {
-					ReqIF10Util.stingToCalendar(value.toString());
-				} catch (ParseException e) {
-					return "Required Format: "
-							+ DateFormat.getDateInstance().format(new Date());
-				} catch (DatatypeConfigurationException e) {
-					return "Parsing Problem: " + e.toString();
-				}
-				return null;
-			}
-		});
+		super(agileGrid);
+		this.editingDomain = editingDomain;
+		this.affectedObject = affectedObject;
+		this.parent = parent;
 	}
 	
 	@Override
 	protected Object doGetValue() {
-		try {
-			GregorianCalendar value = ReqIF10Util.stingToCalendar(text.getText());
+			GregorianCalendar value = new GregorianCalendar(date.getYear(),
+					date.getMonth(), date.getDay(), time.getHours(),
+					time.getMinutes(), time.getSeconds());
 			ProrUtil.setTheValue(attributeValue, value, parent, affectedObject,
 					editingDomain);
-		} catch (ParseException e) {
-			e.printStackTrace();
-			// No action necessary, we simply restore the old value.
-		} catch (DatatypeConfigurationException e) {
-			e.printStackTrace();
-			// No action necessary, we simply restore the old value.
-		}
 		return attributeValue;
 	}
 
 	@Override
 	protected void doSetValue(Object value) {
-		if (value instanceof AttributeValue) {
-			attributeValue = (AttributeValue) value;
+		if (value instanceof AttributeValueDate) {
+			this.attributeValue = (AttributeValueDate) value;
+		}
+		super.doSetValue(value);
+	}
 
-			Object cal = ReqIF10Util.getTheValue(attributeValue);
-			if (cal instanceof GregorianCalendar) {
-				text.setText(DateFormat.getDateInstance().format(
-						((GregorianCalendar) cal).getTime()));
+	@Override
+	protected Control createContents(Composite parent) {
+		Composite composite = new Composite(parent, SWT.BORDER_SOLID);
+		GridLayout layout = new GridLayout();
+		layout.marginHeight = 0;
+		layout.marginWidth = 0;
+		layout.horizontalSpacing = 0;
+		layout.verticalSpacing = 5;
+		composite.setLayout(layout);
+		
+		date = new DateTime(composite, SWT.CALENDAR | SWT.MEDIUM);
+		
+		time = new DateTime(composite, SWT.TIME);
+		GridData gd = new GridData(SWT.FILL, SWT.FILL, true, false);
+		time.setLayoutData(gd);
+
+		Object cal = ReqIF10Util.getTheValue(attributeValue);
+		if (cal instanceof GregorianCalendar) {
+			GregorianCalendar calendar = ((GregorianCalendar) cal);
+			date.setDate(calendar.get(Calendar.YEAR),
+					calendar.get(Calendar.DAY_OF_MONTH), Calendar.DAY_OF_MONTH);
+			time.setTime(calendar.get(Calendar.HOUR),
+					calendar.get(Calendar.MINUTE), Calendar.SECOND);
+		}
+
+		return composite;
+	}
+	
+	/**
+	 * This method is overridden to add a read-and-dispatch loop after opening
+	 * the popup. Otherwise, the panel will disappear immediately on Linux.
+	 */
+	@Override
+	protected void openPopupBox(Control parent) {
+		super.openPopupBox(parent);
+
+		// Workaround to prevent shell from closing right after opening on Linux.
+		Display display = Display.getCurrent();
+		Shell shell = display.getActiveShell();
+		shell.pack();
+
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
 			}
 		}
 	}
