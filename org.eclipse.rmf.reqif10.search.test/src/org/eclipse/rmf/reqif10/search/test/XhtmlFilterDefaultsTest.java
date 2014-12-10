68d3d5b518fd890094f4e3a296ffa13c78ffc2dc
Ingo Weigelt <iw@wglt.de>
2014-12-10 23:10:56 +0100

    Tests for default fallback in XHTMLFilter

---
+++ org.eclipse.rmf.reqif10.search.test/src/org/eclipse/rmf/reqif10/search/test/XhtmlFilterDefaultsTest.java
@@ -0,0 +1,51 @@
+package org.eclipse.rmf.reqif10.search.test;
+
+import static org.junit.Assert.fail;
+
+import java.net.URISyntaxException;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.rmf.reqif10.AttributeDefinitionXHTML;
+import org.eclipse.rmf.reqif10.ReqIF;
+import org.eclipse.rmf.reqif10.search.testdata.TestData;
+import org.junit.Test;
+
+public class XhtmlFilterDefaultsTest extends XhtmlFilterTest {
+
+	@Test
+	public void testOnEmptyAttribute() throws Exception {
+		// Do nothing, since this test will fall back on default values 
+	}
+	
+	
+	/*
+	 * The Value in this reqif is:
+	 * <xhtml:div><xhtml:p style="text-align: left">hello</xhtml:p><xhtml:p style=" text-align: left">world</xhtml:p></xhtml:div>
+	 * 
+	 * (non-Javadoc)
+	 * @see org.eclipse.rmf.reqif10.search.test.AbstractFilterTest#createFixture(java.lang.Object)
+	 */
+	@Override
+	public void createFixture(Object value) {
+		try {
+			URI uri = TestData.getURI("xhtml_with_defaultValues.reqif");
+			//URI uri = TestData.getURI("simplexhtml.reqif");
+			final Resource resource = editingDomain.getResourceSet().getResource(uri, true);
+			final ReqIF reqif = (ReqIF) resource.getContents().get(0);
+			specObject = reqif.getCoreContent().getSpecObjects().get(0);
+			
+			attributeDefinition = (AttributeDefinitionXHTML) reqif.getCoreContent().getSpecTypes().get(0).getSpecAttributes().get(1);
+			
+			if (value == null){
+				attributeDefinition.getDefaultValue().setTheValue(null);
+			}
+			
+			setFixture(specObject);
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+			fail("Could not load testdata");
+		}
+	}
+
+}
