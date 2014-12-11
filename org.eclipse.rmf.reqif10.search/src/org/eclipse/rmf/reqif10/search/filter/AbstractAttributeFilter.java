62c7af2e6768820cc09d930b0f27aee800bf5688
Ingo Weigelt <iw@wglt.de>
2014-12-11 20:17:47 +0100

    an abstract IFilter that can be run on any AttributeDefinition
    the operators IS_SET and IS_NOT do not depend on the type of the
    attributeDefinition

---
+++ org.eclipse.rmf.reqif10.search/src/org/eclipse/rmf/reqif10/search/filter/AbstractAttributeFilter.java
@@ -0,0 +1,70 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Formal Mind GmbH.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Ingo Weigelt - initial API and implementation
+ ******************************************************************************/
+
+package org.eclipse.rmf.reqif10.search.filter;
+
+import org.eclipse.rmf.reqif10.AttributeDefinition;
+import org.eclipse.rmf.reqif10.SpecElementWithAttributes;
+import org.eclipse.rmf.reqif10.common.util.ReqIF10Util;
+
+/**
+ * implementation of operators that can be run on any {@link AttributeDefinition}
+ * 
+ * @author Ingo Weigelt
+ */
+public abstract class AbstractAttributeFilter implements IFilter {
+	
+		
+	public AbstractAttributeFilter(){
+	}
+	
+	
+	@Override
+	public boolean match(SpecElementWithAttributes element) {
+		Object attributeValue = null;
+		
+		
+		if (getAttribute() instanceof AttributeDefinition) {
+			attributeValue = ReqIF10Util.getAttributeValue(element, (AttributeDefinition) getAttribute());
+		}else{
+			attributeValue = getInternalAttributeValue(element);
+		}
+		
+		
+		switch (getOperator()) {
+		case IS_SET:
+			return attributeValue != null;
+		case IS_NOT_SET:
+			return attributeValue == null;
+		default:
+			throw new IllegalArgumentException(
+					"This filter does not support the " + getOperator()
+							+ " operation");
+		}
+	}
+
+	/**
+	 * Filters that can be applied to internalAttributes have to implement this
+	 * class and return a non-null value if the internalAttribute should be
+	 * considered as SET.
+	 * Otherwise they have to return null
+	 * 
+	 * @param element
+	 * @param attribute
+	 * @return the value of the internal attribute or null if the attribute is
+	 *         not set
+	 */
+	protected Object getInternalAttributeValue(
+			SpecElementWithAttributes element){
+		return null;
+	}
+	
+}
