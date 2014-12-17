7b7520b14979716c9e2ea6eea89a815f7a8dec61
Michael Jastram <michael@jastram.de>
2014-12-17 21:00:49 +0100

    Refactored FilterControls, moving some logic in the superclass.

--- org.eclipse.rmf.reqif10.search.ui/src/org/eclipse/rmf/reqif10/search/filter/ui/FilterControlEnum.java
+++ org.eclipse.rmf.reqif10.search.ui/src/org/eclipse/rmf/reqif10/search/filter/ui/FilterControlEnum.java
@@ -10,10 +10,12 @@
  ******************************************************************************/
 package org.eclipse.rmf.reqif10.search.filter.ui;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.rmf.reqif10.AttributeDefinitionEnumeration;
 import org.eclipse.rmf.reqif10.EnumValue;
 import org.eclipse.rmf.reqif10.pror.editor.presentation.EnumSelector;
@@ -24,9 +26,7 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
 
@@ -38,57 +38,65 @@ import org.eclipse.swt.widgets.Shell;
 public class FilterControlEnum extends FilterControl {
 	
 	private Button valueControl;
-	private Combo attr;
-	private AttributeDefinitionEnumeration attribute;
-	private EnumFilter templateFilter;
 	private Collection<EnumValue> items;
 
 	public FilterControlEnum(FilterPanel parent,
 			AttributeDefinitionEnumeration attribute) {
-		super(parent, SWT.FLAT);
-		this.attribute = attribute;
-		init();
+		super(parent, attribute);
 	}
 
 	public FilterControlEnum(FilterPanel parent, EnumFilter template) {
-		super(parent, SWT.FLAT);
-		this.attribute = (AttributeDefinitionEnumeration) template.getAttribute();
-		this.templateFilter = template;			
-		init();		
+		super(parent, template);
 	}
 
-	private void init() {
-		if (!(attribute instanceof AttributeDefinitionEnumeration)) {
-			throw new IllegalArgumentException("Not allowed: " + attribute);
-		}
-		setLayout(new GridLayout(2, false));
-		if (templateFilter != null) {
-			items = templateFilter.getFilterValue1();
+	@Override
+	protected List<Operator> getOperators() {
+		return EnumFilter.SUPPORTED_OPERATORS.asList();
+	}
+
+	@Override
+	protected void updateValueControls(boolean initialize) {
+		if (getOperator() == Operator.IS_SET
+				|| getOperator() == Operator.IS_NOT_SET) {
+			showControl(false);
 		} else {
-			items = new HashSet<EnumValue>();
+			showControl(true);
 		}
-		createOperators();
-		createValueControl();
-		updateItems(items);
+		if (initialize) {
+			if (valueControl != null) {
+				updateItems(((EnumFilter)templateFilter).getFilterValue1());
+			}
+		}
 	}
 
-	private void createValueControl() {
-		valueControl = new Button(this, SWT.PUSH | SWT.FLAT | SWT.WRAP);
-		GridData layoutData = new GridData(SWT.FILL, SWT.CENTER, true, false);
-		layoutData.widthHint = 200;
-		valueControl.setLayoutData(layoutData);
-		valueControl.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				popupSelector(valueControl);
-			}
-		});
+	private void showControl(boolean show) {
+		if (show && valueControl == null) {
+			if (items == null) items = new ArrayList<EnumValue>();
+
+			valueControl = new Button(this, SWT.PUSH | SWT.FLAT | SWT.WRAP);
+			GridData layoutData = new GridData(SWT.FILL, SWT.CENTER, true, false);
+			layoutData.widthHint = 200;
+			valueControl.setLayoutData(layoutData);
+			valueControl.addSelectionListener(new SelectionAdapter() {
+				@Override
+				public void widgetSelected(SelectionEvent e) {
+					popupSelector(valueControl);
+				}
+			});
+		}
+		if (!show && valueControl != null) {
+			valueControl.dispose();
+			valueControl = null;
+			items = null;
+		}
 	}
 
 	protected void popupSelector(Control control) {
+		EList<EnumValue> specifiedValues = ((AttributeDefinitionEnumeration) attribute)
+				.getType().getSpecifiedValues();
 		Shell shell = EnumSelector.createShell(control);
-		EnumSelector selector = new EnumSelector(attribute.getType()
-				.getSpecifiedValues(), items, shell, SWT.BORDER);
+		EnumSelector selector = new EnumSelector(specifiedValues, items, shell,
+				SWT.BORDER);
 		int status = selector.showEnumSelector(shell);
 		if (status == SWT.OK) {
 			updateItems(selector.getItems());
@@ -96,13 +104,15 @@ public class FilterControlEnum extends FilterControl {
 	}
 
 	private void updateItems(Collection<EnumValue> items) {
+		EList<EnumValue> specifiedValues = ((AttributeDefinitionEnumeration) attribute)
+				.getType().getSpecifiedValues();
 		this.items = items;
 		StringBuilder sb = new StringBuilder();
 		for (Iterator<EnumValue> i = items.iterator(); i.hasNext();) {
 			EnumValue value = i.next();
 
 			// In case items have been removed
-			if (! attribute.getType().getSpecifiedValues().contains(value)) continue;
+			if (! specifiedValues.contains(value)) continue;
 
 			String label = value.getLongName() != null ? value.getLongName() : value.getIdentifier(); 
 			sb.append(label);
@@ -114,22 +124,9 @@ public class FilterControlEnum extends FilterControl {
 		getParent().getParent().layout();
 	}
 
-	// TODO use correct operators.
-	private void createOperators() {
-		attr = new Combo(this, SWT.DROP_DOWN | SWT.BORDER | SWT.READ_ONLY);
-		GridData layoutData = new GridData(SWT.LEFT, SWT.CENTER, false, false);
-		attr.setLayoutData(layoutData);
-		for (Operator operator : EnumFilter.SUPPORTED_OPERATORS) {
-			attr.add(operator.toLocaleString());			
-		}
-		attr.select(0);
-		if (templateFilter != null)
-			attr.select(EnumFilter.SUPPORTED_OPERATORS.asList().indexOf(
-					templateFilter.getOperator()));
-	}
-	
 	public IFilter getFilter() {
-		Operator operator = EnumFilter.SUPPORTED_OPERATORS.asList().get(attr.getSelectionIndex());
-		return new EnumFilter(operator, items, attribute);
+		return new EnumFilter(getOperator(), items,
+				(AttributeDefinitionEnumeration) attribute);
 	}
+
 }
