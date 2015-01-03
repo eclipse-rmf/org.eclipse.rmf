99cb33611b0df2d4fc2da32da424f3b1805030f7
Michael Jastram <michael@jastram.de>
2015-01-03 20:44:38 +0100

    Fix bug in Date Filter restauration code.

--- org.eclipse.rmf.reqif10.search.ui/src/org/eclipse/rmf/reqif10/search/filter/ui/FilterControlDate.java
+++ org.eclipse.rmf.reqif10.search.ui/src/org/eclipse/rmf/reqif10/search/filter/ui/FilterControlDate.java
@@ -61,11 +61,11 @@ public class FilterControlDate extends FilterControl {
 		}
 		
 		if (initialize) {
-			if (templateFilter.getFilterValue1() != null) {
+			if (control[0] != null) {
 				GregorianCalendar cal = (GregorianCalendar) templateFilter.getFilterValue1();
 				control[0].setDate(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
 			}
-			if (templateFilter.getFilterValue2() != null) {
+			if (control[1] != null) {
 				GregorianCalendar cal = (GregorianCalendar) templateFilter.getFilterValue2();
 				control[1].setDate(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
 			}
