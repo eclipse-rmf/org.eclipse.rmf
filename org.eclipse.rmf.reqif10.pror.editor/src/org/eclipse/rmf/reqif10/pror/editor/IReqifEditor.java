718fa25183a9dd6b4cc6e9053fffe8f630a11e36
Michael Jastram <michael@jastram.de>
2015-01-15 11:02:07 +0100

    Interfaces for ReqIF Editors.

---
+++ org.eclipse.rmf.reqif10.pror.editor/src/org/eclipse/rmf/reqif10/pror/editor/IReqifEditor.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Formal Mind GmbH.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Michael Jastram - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.rmf.reqif10.pror.editor;
+
+import org.eclipse.emf.edit.domain.IEditingDomainProvider;
+
+/**
+ * This interface must be implemented by all editors for ReqIF files.
+ * 
+ * @author jastram
+ */
+public interface IReqifEditor extends IEditingDomainProvider {
+
+}
---
+++ org.eclipse.rmf.reqif10.pror.editor/src/org/eclipse/rmf/reqif10/pror/editor/ISpecificationEditor.java
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Formal Mind GmbH.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Michael Jastram - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.rmf.reqif10.pror.editor;
+
+/**
+ * This interface must be implemented by all editors for ReqIF Specifications.
+ * 
+ * @author jastram
+ */
+public interface ISpecificationEditor extends IReqifEditor {
+
+}
