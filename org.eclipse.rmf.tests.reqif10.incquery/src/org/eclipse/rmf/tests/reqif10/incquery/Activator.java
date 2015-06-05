169e6f79eeb0fb3a888ea96948c09d5bdd56956d
Mark Broerkens <mark.broerkens@itemis.de>
2015-06-05 08:17:25 +0200

    469356: Poor performance in Case of Big Files with many SpecRelations
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=469356

---
+++ org.eclipse.rmf.tests.reqif10.incquery/src/org/eclipse/rmf/tests/reqif10/incquery/Activator.java
@@ -0,0 +1,74 @@
+package org.eclipse.rmf.tests.reqif10.incquery;
+
+import org.eclipse.emf.common.EMFPlugin;
+import org.eclipse.emf.common.util.ResourceLocator;
+
+/**
+ * This is the central singleton for this plug-in.
+ */
+public final class Activator extends EMFPlugin {
+
+	/**
+	 * Keep track of the singleton.
+	 */
+	public static final Activator INSTANCE = new Activator();
+
+	/**
+	 * Keep track of the singleton.
+	 */
+	private static Implementation plugin;
+
+	/**
+	 * Create the instance.
+	 */
+	public Activator() {
+		super(new ResourceLocator[] {});
+	}
+
+	/**
+	 * Returns the singleton instance of the Eclipse plug-in.
+	 * 
+	 * @return the singleton instance.
+	 */
+	@Override
+	public ResourceLocator getPluginResourceLocator() {
+		return plugin;
+	}
+
+	/**
+	 * Returns the singleton instance of the Eclipse plug-in.
+	 * 
+	 * @return the singleton instance.
+	 */
+	public static Implementation getPlugin() {
+		return plugin;
+	}
+
+	/**
+	 * Returns the singleton instance of the Eclipse plug-in. This method does actually the same thing as getPlugin()
+	 * and has been put in place for compatibility reasons with Activator classes which are not EMF-based but generated
+	 * by PDE.
+	 * 
+	 * @return the singleton instance.
+	 */
+	public static Implementation getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * The actual implementation of the Eclipse <b>Plugin</b>.
+	 */
+	public static class Implementation extends EclipsePlugin {
+		/**
+		 * Creates an instance.
+		 */
+		public Implementation() {
+			super();
+
+			// Remember the static instance.
+			//
+			plugin = this;
+		}
+	}
+}
+
---
+++ org.eclipse.rmf.tests.reqif10.incquery/src/org/eclipse/rmf/tests/reqif10/incquery/NavigationServiceTest.java
@@ -0,0 +1,78 @@
+package org.eclipse.rmf.tests.reqif10.incquery;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.eclipse.incquery.runtime.api.IncQueryEngine;
+import org.eclipse.incquery.runtime.api.scope.IncQueryScope;
+import org.eclipse.incquery.runtime.exception.IncQueryException;
+import org.eclipse.rmf.reqif10.ReqIF;
+import org.eclipse.rmf.reqif10.SpecObject;
+import org.eclipse.rmf.reqif10.incquery.SpecRelationsMatch;
+import org.eclipse.rmf.reqif10.incquery.SpecRelationsMatcher;
+import org.eclipse.sphinx.emf.serialization.XMLPersistenceMappingResourceSetImpl;
+import org.junit.Test;
+
+public class NavigationServiceTest {
+
+	@Test
+	public void test() throws IOException {
+		ResourceSet resourceSet = new XMLPersistenceMappingResourceSetImpl();
+	    URI fileURI = URI.createPlatformPluginURI(Activator.getPlugin().getBundle()
+				.getSymbolicName()
+				+ "/resources/input/out.reqif", false);
+		XMLResource resource = (XMLResource)resourceSet.getResource(fileURI, true);
+		resource.load(null);
+		resource.getEObject("not found");
+		
+		assertSame(1,resource.getContents().size());
+		
+		ReqIF reqIF = (ReqIF)resource.getContents().get(0) ;
+		
+		try {
+			// get all matches of the pattern
+			// initialization
+			// phase 1: (managed) IncQueryEngine
+			IncQueryEngine engine = IncQueryEngine.on(reqIF);
+			
+			// phase 2: the matcher itself
+			SpecRelationsMatcher matcher = SpecRelationsMatcher.on(engine);
+			
+			for (SpecObject specObject : reqIF.getCoreContent().getSpecObjects()) {
+				Collection<SpecRelationsMatch> incomingMatches = matcher.getAllMatches(null, null, specObject);
+				int incomingCount = incomingMatches.size();
+				
+				Collection<SpecRelationsMatch> outgoingMatches = matcher.getAllMatches(null, specObject, null);
+				int outgoingCount = outgoingMatches.size();
+				
+				System.out.println(specObject.getIdentifier() + "  incoming " + incomingCount + ", outgoing: " + outgoingCount);
+				
+			}
+			
+			// get all matches of the pattern
+			Collection<SpecRelationsMatch> matches = matcher.getAllMatches();
+
+			System.out.println("Number of Matches: " + matches.size());
+			// using a match processor
+			/*
+			matcher.forEachMatch(new EObjectProcessor() {
+				@Override
+				public void process(EObject o) {
+					results.append("\tEObject: " + o.toString() + "\n");
+				}
+			});
+			*/
+		} catch (IncQueryException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
